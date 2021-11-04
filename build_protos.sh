34399b083d3fa03608564fc8b99fa6313e7041cc
unconst <jake@bittensor.com>
2021-11-04 15:41:02 -0500

    initial commit

---
+++ build_protos.sh
@@ -0,0 +1,1 @@
+python3 -m grpc.tools.protoc metagraph.proto  -I. --python_out=. --grpc_python_out=.
---
+++ main.py
@@ -0,0 +1,41 @@
+
+import bittensor
+import pickle
+from concurrent import futures
+
+import grpc
+import metagraph_pb2
+import metagraph_pb2_grpc
+
+class MetagraphServicer ( metagraph_pb2_grpc.MetagraphServer ):
+    def __init__(self):
+        graph = bittensor.metagraph()
+        graph.load()
+        self.bytes = pickle.dumps( graph.state_dict() )
+
+    def Get( self, request: metagraph_pb2.Block, context: grpc.ServicerContext  ) -> metagraph_pb2.Metagraph:
+        return metagraph_pb2.Metagraph ( bytes = self.bytes )
+
+if __name__ == '__main__':
+
+    # Create server.
+    thread_pool = futures.ThreadPoolExecutor( max_workers = 10 )
+    metagraph_server = grpc.server( thread_pool )
+    metagraph_servicer = MetagraphServicer()
+    metagraph_pb2_grpc.add_MetagraphServerServicer_to_server( metagraph_servicer, metagraph_server )
+    metagraph_server.add_insecure_port( '127.0.0.1:7869' )
+    metagraph_server.start()
+
+    # Connections.
+    channel = grpc.insecure_channel( '127.0.0.1:7869' )
+    stub = metagraph_pb2_grpc.MetagraphServerStub( channel )
+    request = metagraph_pb2.Block ()
+    graph = bittensor.metagraph()
+    state = pickle.loads(stub.Get( request = request ).bytes)
+    graph.load_from_state_dict( state )
+    print( graph )
+
+
+
+
+
